b67621a86bdb3db5f6dca4c68058f7beb59dfccd
xushuyang <xushuyang@foton.com.cn>
2018-02-12 14:13:38 +0800

    edit

--- com-github-app-ui/src/views/layout/components/Sidebar/SidebarItem.vue
+++ com-github-app-ui/src/views/layout/components/Sidebar/SidebarItem.vue
@@ -3,12 +3,11 @@
     <template v-for="item in routes" v-if="item.children">
 
       <router-link v-if="item.children.length===1 && !item.children[0].children" :to="item.path+item.children[0].path" :key="item.name">
-        <el-menu-item :index="item.path+'/'+item.children[0].path" :class="{'submenu-title-noDropdown':!isNest}">
+        <el-menu-item :index="item.path+'/'+item.children[0].path" :class="{'submenu-title-noDropdown':true}">
           <svg-icon v-if="item.icon" :icon-class="item.icon"></svg-icon>
           <span v-if="item.name">{{item.name}}</span>
         </el-menu-item>
       </router-link>
-
 
       <el-submenu v-else :index="item.name||item.path" :key="item.name">
         <template slot="title">
